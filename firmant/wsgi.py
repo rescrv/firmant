66e30c968be5ed6d4daad76a56fef95f2554fdc7
Robert Escriva <me@robescriva.com>
2008-10-30 18:38:23 -0400

    Factored wsgi interface into firmant.wsgi
    I'm not sure if Request and Response belong here, but it is much better than
    having them pollute the namespace in firmant/__init__.py.

---
+++ firmant/wsgi.py
@@ -0,0 +1,32 @@
+class Request(object):
+    def __init__(self, environ=None):
+        self.method = environ["REQUEST_METHOD"]
+        self.url_prefix = environ["SCRIPT_NAME"]
+        self.url = environ["PATH_INFO"]
+
+class Response(object):
+    def __init__(self,
+            status='200 OK',
+            headers=[('content-type', 'text/plain')],
+            content=''):
+        self.status = status
+        self.headers = headers
+        self.content = content
+
+class Application(object):
+    def __init__(self, environ, start_response):
+        self.environ = environ
+        self.start = start_response
+
+    def __iter__(self):
+        '''
+        Proceeds in three stages:
+        1. Environ will be parsed into a Request instance.
+        2. Handlers are passed the Request until a Response is returned.
+        3. The response is returned to the WSGI Server
+        '''
+        request = Request(self.environ)
+        response = Response('200 OK', [('content-type', 'text/plain')],
+                request.url)
+        self.start(response.status, response.headers)
+        yield response.content
--- runserver.py
+++ runserver.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 from wsgiref.simple_server import make_server
-from firmant import Application
+from firmant.wsgi import Application
 
 make_server('', 8080, Application).serve_forever()
