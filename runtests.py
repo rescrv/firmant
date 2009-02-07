1714132b319e3193c231c2c251326dbc317b9053
Robert Escriva <me@robescriva.com>
2009-02-07 18:59:59 -0500

    Changed settings to settings_tests in runtests.py
    This will avoid a collision with the name space in the future when
    runtests imports settings to configure it.

--- runtests.py
+++ runtests.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 import unittest
 
-from test.settings import suite as settings
+from test.settings import suite as settings_tests
+
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
-    suite.addTests(settings)
+    suite.addTests(settings_tests)
     unittest.TextTestRunner(verbosity=2).run(suite)
