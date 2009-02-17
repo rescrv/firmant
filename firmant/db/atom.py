52329d0f805e6fd5e4a46f50ae8e74f410392d52
Robert Escriva <me@robescriva.com>
2009-02-16 22:01:40 -0500

    Refactored Entry select code.
    We can reuse the same select code if we play a few clever tricks.

--- firmant/db/atom.py
+++ firmant/db/atom.py
@@ -55,21 +55,6 @@ class Entry(Relation):
 
     @classmethod
     def single(cls, slug, date):
-        sql = """
-        SELECT e.slug, e.published_date, e.published_time, p.name, p.uri,
-               p.email, ca.term, ca.label, e.rights, er.updated, er.title,
-               co.content, co.summary
-        FROM entries e, people p, categories ca, entry_revisions er, content co
-        WHERE e.author = p.name AND
-              e.category = ca.term AND
-              e.published_date = %(pub_date)s AND
-              e.slug = %(slug)s AND
-              er.slug = e.slug AND
-              er.published_date = e.published_date AND
-              er.content = co.id
-        ORDER BY er.updated DESC
-        LIMIT 1;
-        """
         if slug_re.match(slug) == None:
             raise ValueError('Invalid slug')
         try:
@@ -85,15 +70,12 @@ class Entry(Relation):
 
         conn = AtomDB.connection(readonly=True)
         cur = conn.cursor()
-
         try:
-            cur.execute('SET search_path = atom;')
-            cur.execute(sql, {'pub_date': date, 'slug': slug})
+            cur = cls._posts_in_range(cur, slug, datestr, datestr)
         except psycopg2.DataError, e:
             cur.close()
             conn.close()
             raise ValueError('Invalid date')
-
         results = cls._select(cur, cls.attributes)
         cur.close()
         conn.close()
@@ -103,3 +85,33 @@ class Entry(Relation):
             return results[0]
         else:
             raise ValueError("A single select returned multiple")
+
+    @classmethod
+    def _posts_in_range(cls, cur, slug, min, max):
+        additional_compare = {'min_pub_date': '%(min_pub_date)s',
+                              'max_pub_date': '%(max_pub_date)s',
+                              'slugselect': ''}
+        params = {'min_pub_date': min, 'max_pub_date': max}
+        if slug != None:
+            additional_compare['slugselect'] = 'e.slug = %(slug)s'
+            params['slug'] = slug
+        sql = """
+        SELECT e.slug, e.published_date, e.published_time, p.name, p.uri,
+               p.email, ca.term, ca.label, e.rights, er.updated, er.title,
+               co.content, co.summary
+        FROM entries e, people p, categories ca, entry_revisions er, content co
+        WHERE e.author = p.name AND
+              e.category = ca.term AND
+              e.published_date <= %(max_pub_date)s AND
+              e.published_date >= %(min_pub_date)s AND
+              er.slug = e.slug AND
+              er.published_date = e.published_date AND
+              er.content = co.id AND
+              %(slugselect)s
+        ORDER BY er.updated DESC
+        LIMIT 1;
+        """ % additional_compare
+        cur.execute('SET search_path = atom;')
+        cur.execute(sql, params)
+
+        return cur
