d2d9dc255d2737349a12e2da726223bd5687a32c
Robert Escriva <me@robescriva.com>
2009-02-17 11:44:56 -0500

    Added another entry to sample data.
    The new entry is on 2009-2-17 and is larger.  It also will help with
    debugging the testEntry* functions.

--- firmant/db/schemas/atom-sample-data.sql
+++ firmant/db/schemas/atom-sample-data.sql
@@ -18,14 +18,49 @@ INSERT INTO content (id, content, summary) VALUES
      'This is the main content of revision two.',
      'This is the summary of revision two.');
 
+INSERT INTO content (id, content, summary) VALUES
+    (3,
+     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget
+     ante sit amet elit condimentum varius. Nullam blandit pede quis neque.
+     Suspendisse elit erat, malesuada quis, ultrices cursus, pretium eget,
+     tortor. Sed volutpat pede in neque rhoncus aliquet. In vulputate, tellus id
+     scelerisque vestibulum, eros diam vehicula massa, ac mollis leo augue quis
+     tortor. Vivamus sed eros vitae tortor tincidunt consequat. Morbi in erat
+     non erat tristique accumsan. Quisque ornare libero ut turpis. Phasellus et
+     tortor. Vestibulum dapibus aliquet sapien. Quisque ut ante in erat auctor
+     accumsan. Morbi accumsan dolor ut est.
+
+     Nam urna lectus, auctor vel, placerat ac, fringilla interdum, leo. Donec
+     congue venenatis lorem. Mauris pellentesque venenatis est. Mauris nisl
+     dolor, ultrices a, condimentum vel, consequat sit amet, risus. Nulla
+     porttitor nisl ut magna venenatis commodo. Donec sagittis leo in neque. Cum
+     sociis natoque penatibus et magnis dis parturient montes, nascetur
+     ridiculus mus. In hac habitasse platea dictumst. Praesent libero velit,
+     volutpat sit amet, molestie id, condimentum nec, lorem. Aliquam erat
+     volutpat. Vivamus scelerisque purus nec nulla. Aliquam tortor. Suspendisse
+     laoreet porta augue. Curabitur at sapien in enim consequat blandit. Nulla
+     ac dui. Nunc felis est, tempor sit amet, tincidunt id, auctor quis, eros.
+     In molestie est a neque. Aliquam erat volutpat. Nam commodo tincidunt
+     magna.  Morbi justo leo, faucibus nec, consectetur id, sodales vitae,
+     nunc.',
+     'A generated loren ipsum paragraph.');
+
 -- Insert a sample person (me).  Not my actual email
 INSERT INTO people (name, uri, email) VALUES
     ('Robert Escriva',
      'http://robescriva.com',
      'rob@/dev/null');
 
+INSERT INTO people (name, uri, email) VALUES
+    ('Loren Ipsum Generator',
+     'http://www.lipsum.com',
+     'lipsum@/dev/null');
+
 -- Insert a general category.
 INSERT INTO categories (term, label) VALUES ('General', 'All topics');
+INSERT INTO categories (term, label) VALUES
+    ('Generated',
+     'You can\'t tell a computer wrote it.');
 
 -- Insert the test entry and its two revisions.
 INSERT INTO entries
@@ -35,6 +70,15 @@ INSERT INTO entries
      '233130 EST',
      'Robert Escriva',
      'General',
+     'Same as source.');
+
+INSERT INTO entries
+    (slug, published_date, published_time, author, category, rights) VALUES
+    ('loren-ipsum',
+     '2009-2-17',
+     '113130 EST',
+     'Loren Ipsum Generator',
+     'Generated',
      'Same as source.');
 
 INSERT INTO entry_revisions
@@ -52,3 +96,11 @@ INSERT INTO entry_revisions
      '2009-2-13 233131 EST',
      'Unix 1234567890',
      2);
+
+INSERT INTO entry_revisions
+    (slug, published_date, updated, title, content) VALUES
+    ('loren-ipsum',
+     '2009-2-17',
+     '2009-2-17 113130 EST',
+     'Loren Ipsum ...',
+     3);
--- test/db/atom.py
+++ test/db/atom.py
@@ -28,6 +28,44 @@ class TestAtomSchema(unittest.TestCase):
         self.e1.content        = 'This is the main content of revision two.'
         self.e1.summary        = 'This is the summary of revision two.'
 
+        self.e2 = Entry()
+        self.e2.slug           = 'loren-ipsum'
+        self.e2.published_date = datetime.date(2009, 2, 17)
+        self.e2.published_time = datetime.time(11, 31, 30)
+        self.e2.author_name    = 'Loren Ipsum Generator'
+        self.e2.author_uri     = 'http://www.lipsum.com'
+        self.e2.author_email   = 'lipsum@/dev/null'
+        self.e2.category_term  = 'Generated'
+        self.e2.category_label = "You can't tell a computer wrote it."
+        self.e2.rights         = 'Same as source.'
+        self.e2.updated        = datetime.datetime(2009, 2, 17, 11, 31, 30)
+        self.e2.title          = 'Loren Ipsum ...'
+        self.e2.content        = (
+     """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget
+     ante sit amet elit condimentum varius. Nullam blandit pede quis neque.
+     Suspendisse elit erat, malesuada quis, ultrices cursus, pretium eget,
+     tortor. Sed volutpat pede in neque rhoncus aliquet. In vulputate, tellus id
+     scelerisque vestibulum, eros diam vehicula massa, ac mollis leo augue quis
+     tortor. Vivamus sed eros vitae tortor tincidunt consequat. Morbi in erat
+     non erat tristique accumsan. Quisque ornare libero ut turpis. Phasellus et
+     tortor. Vestibulum dapibus aliquet sapien. Quisque ut ante in erat auctor
+     accumsan. Morbi accumsan dolor ut est.
+
+     Nam urna lectus, auctor vel, placerat ac, fringilla interdum, leo. Donec
+     congue venenatis lorem. Mauris pellentesque venenatis est. Mauris nisl
+     dolor, ultrices a, condimentum vel, consequat sit amet, risus. Nulla
+     porttitor nisl ut magna venenatis commodo. Donec sagittis leo in neque. Cum
+     sociis natoque penatibus et magnis dis parturient montes, nascetur
+     ridiculus mus. In hac habitasse platea dictumst. Praesent libero velit,
+     volutpat sit amet, molestie id, condimentum nec, lorem. Aliquam erat
+     volutpat. Vivamus scelerisque purus nec nulla. Aliquam tortor. Suspendisse
+     laoreet porta augue. Curabitur at sapien in enim consequat blandit. Nulla
+     ac dui. Nunc felis est, tempor sit amet, tincidunt id, auctor quis, eros.
+     In molestie est a neque. Aliquam erat volutpat. Nam commodo tincidunt
+     magna.  Morbi justo leo, faucibus nec, consectetur id, sodales vitae,
+     nunc.""")
+        self.e2.summary        = 'A generated loren ipsum paragraph.'
+
     def tearDown(self):
         # We do not want settings in here to affect any other tests.
         AtomDB.reset()
@@ -77,6 +115,8 @@ class TestAtomSchema(unittest.TestCase):
         conn.close()
         e = Entry.single('sample', datetime.date(2009, 2, 13))
         self.assertEqual(e, self.e1)
+        e = Entry.single('loren-ipsum', datetime.date(2009, 2, 17))
+        self.assertEqual(e, self.e2)
 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestAtomSchema)
