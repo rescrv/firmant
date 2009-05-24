ce507b1727eb95ffc66f5af54c4c88a596384de0
Robert Escriva <me@robescriva.com>
2009-05-24 01:32:54 -0400

    PluginMount class

--- firmant/plugins.py
+++ firmant/plugins.py
@@ -3,6 +3,26 @@ from firmant.backend.atom import Entry
 from firmant.utils import get_module
 
 
+class PluginMount(type):
+    """
+    Designed by Marty Alchin in his 'A Simple Plugin Framework' article:
+    http://martyalchin.com/2008/jan/10/simple-plugin-framework/
+    """
+
+    def __init__(cls, name, bases, attrs):
+        if not hasattr(cls, 'plugins'):
+            # This branch only executes when processing the mount point itself.
+            # So, since this is a new plugin type, not an implementation, this
+            # class shouldn't be registered as a plugin. Instead, it sets up a
+            # list where plugins can be registered later.
+            cls.plugins = []
+        else:
+            # This must be a plugin implementation, which should be registered.
+            # Simply appending it to the list is all that's needed to keep
+            # track of it later.
+            cls.plugins.append(cls)
+
+
 resolver_list = []
 
 
