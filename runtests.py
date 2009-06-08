19ece7b72aabb25614e2d904e24aee7c28cdb75a
Robert Escriva <me@robescriva.com>
2009-06-08 12:26:34 -0400

    rm -r flawed frontend tests.
    They will surely be re-added when frontend system is refactored.

--- runtests.py
+++ runtests.py
@@ -8,7 +8,6 @@ from test.configuration import suite as configuration_tests
 from test.db.atom import suite as db_atom_tests
 from test.db.relations import suite as db_relations_tests
 from test.resolvers import suite as resolvers_tests
-from test.frontend.txt.resolvers import suite as txt_resolver_tests
 
 
 if __name__ == '__main__':
@@ -18,5 +17,4 @@ if __name__ == '__main__':
     suite.addTests(db_atom_tests)
     suite.addTests(db_relations_tests)
     suite.addTests(resolvers_tests)
-    suite.addTests(txt_resolver_tests)
     unittest.TextTestRunner(verbosity=2).run(suite)
