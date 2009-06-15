6d6d9f4ddf3fae0e2de6e370b31d936bc61987c6
Robert Escriva <me@robescriva.com>
2009-06-15 12:54:24 -0400

    Base Atom Classes.

--- firmant/datasource/atom.py
+++ firmant/datasource/atom.py
@@ -1,4 +1,114 @@
+import datetime
+
 from firmant.plugins import PluginMount
+from firmant.utils import not_implemented
+
+
+# Base classes for Atom data.
+
+
+class AtomBase(object):
+
+    def __eq__(self, other):
+        for field in self.fields:
+            if hasattr(self, field) \
+               and hasattr(other, field) \
+               and getattr(self, field) != getattr(other, field):
+                return False
+            if hasattr(self, field) \
+               and not hasattr(other, field):
+                return False
+            if not hasattr(self, field) \
+               and hasattr(other, field):
+                return False
+        return True
+
+    def __ne__(self, other):
+        return not __eq__(self, other)
+
+
+class Entry(AtomBase):
+
+    # In case a subclass overloads __slots__ we can still compare two Entry
+    # objects.
+    fields    = ['slug', 'published', 'author', 'category', 'rights', 'updated',
+                 'title', 'content', 'summary', 'tz']
+    __slots__ = fields
+
+    @classmethod
+    def for_feed(cls, feedslug, count=10, offset=0):
+        not_implemented()
+
+    @classmethod
+    def single(cls, slug, date):
+        not_implemented()
+
+    @classmethod
+    def day(cls, year, month, day, count=10, offset=0):
+        not_implemented()
+
+    @classmethod
+    def month(cls, year, month, count=10, offset=0):
+        not_implemented()
+
+    @classmethod
+    def year(cls, year, count=10, offset=0):
+        not_implemented()
+
+    @classmethod
+    def recent(cls, count=10, offset=0):
+        not_implemented()
+
+    def permalink(self):
+        not_implemented()
+
+
+class Feed(AtomBase):
+
+    fields    = ['slug', 'title', 'rights', 'subtitle', 'updated']
+    __slots__ = fields
+
+    @classmethod
+    def by_slug(cls, slug):
+        not_implemented()
+
+    @classmethod
+    def default(cls):
+        not_implemented()
+
+    def permalink(self):
+        not_implemented()
+
+
+class Author(AtomBase):
+
+    fields    = ['name', 'uri', 'email']
+    __slots__ = fields
+
+    @classmethod
+    def by_name(cls, name):
+        not_implemented()
+
+    # TODO:
+    #def permalink(self):
+    #    not_implemented()
+
+
+class Category(AtomBase):
+
+    fields    = ['term', 'label']
+    __slots__ = fields
+
+    @classmethod
+    def by_term(cls, term):
+        not_implemented()
+
+    # TODO:
+    #def permalink(self):
+    #    not_implemented()
+
+
+# Plugin Code
 
 
 def select_canonical_plugin(plugins, settings, config_var):
