72f7fd16df48d0f848420fbfbcee9c5f18cc482c
Robert Escriva <me@robescriva.com>
2009-06-19 21:26:57 -0400

    Removed settings code

--- runtests.py
+++ runtests.py
@@ -4,20 +4,11 @@ from firmant.utils import get_module
 # Import this now to avoid it throwing errors.
 import pytz
 
-from firmant.configuration import settings
-from test.configuration import suite as configuration_tests
 from test.datasource.atom import suite as atom_tests
 from test.plugins.datasource.flatfile.atom import suite as flatfile_atom_tests
 
 if __name__ == '__main__':
-    settings.reconfigure('test_settings')
-    for plugin in settings['PLUGINS']:
-        try:
-            mod = get_module(plugin)
-        except ImportError:
-            raise
     suite = unittest.TestSuite()
-    suite.addTests(configuration_tests)
     suite.addTests(atom_tests)
     suite.addTests(flatfile_atom_tests)
     unittest.TextTestRunner(verbosity=2).run(suite)
