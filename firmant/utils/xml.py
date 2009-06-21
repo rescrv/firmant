be9c10739e89902d2f1e11fb2a914e6dd7741ba3
Robert Escriva <me@robescriva.com>
2009-06-21 17:19:51 -0400

    Some utilities for doing XML work

---
+++ firmant/utils/xml.py
@@ -0,0 +1,24 @@
+try:
+    from lxml import etree
+except ImportError:
+    try:
+        # Python 2.5
+        import xml.etree.cElementTree as etree
+    except ImportError:
+        try:
+            # Python 2.5
+            import xml.etree.ElementTree as etree
+        except ImportError:
+            try:
+                # normal cElementTree install
+                import cElementTree as etree
+            except ImportError:
+                try:
+                    # normal ElementTree install
+                    import elementtree.ElementTree as etree
+                except ImportError:
+                    raise
+
+def add_text_subelement(root, name, text):
+    new = etree.SubElement(root, name)
+    new.text = text
