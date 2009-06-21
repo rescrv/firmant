7200a886b2056c4a6d9a692af0baced786f84bd9
Robert Escriva <me@robescriva.com>
2009-06-20 23:19:29 -0400

    Remove old txt frontend

--- firmant/frontend/txt/__init__.py
+++
@@ -1,7 +0,0 @@
-from firmant.plugins import resolver_list
-from firmant.frontend.txt.resolvers import TxtDateResolver
-from firmant.configuration import settings
-
-
-def load():
-    resolver_list.append(TxtDateResolver(settings['FRONTEND_TXT_PREFIX']))
