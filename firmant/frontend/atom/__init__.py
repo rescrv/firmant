c6f6531aa969b5346dd7b7f759ea498665f40092
Robert Escriva <me@robescriva.com>
2009-06-21 23:42:17 -0400

    New atom view.
    Permalinks not yet provided, but should be coming soon.

--- firmant/frontend/atom/__init__.py
+++
@@ -1,7 +0,0 @@
-from firmant.plugins import resolver_list
-from firmant.frontend.atom.resolvers import AtomResolver
-from firmant.configuration import settings
-
-
-def load():
-    resolver_list.append(AtomResolver(settings['FRONTEND_ATOM_PREFIX']))
---
+++ firmant/plugins/views/atom.py
@@ -0,0 +1,46 @@
+from werkzeug.routing import Rule, \
+                             Submount
+from werkzeug import Response
+from werkzeug.exceptions import NotFound
+
+from firmant.datasource.atom import AtomProvider
+from firmant.datasource.atom import FeedPermalinkProvider
+from firmant.views import ViewProvider
+from firmant.utils import xml
+
+
+class AtomFeedViewProvider(ViewProvider):
+
+    def __init__(self, settings):
+        self.settings = settings
+        self.ap = AtomProvider(settings)
+
+    @property
+    def rules(self):
+        name = __name__ + '.AtomFeedViewProvider.'
+        url_rules = [
+            Rule('/', endpoint=name + 'default'),
+            Rule('/<slug>/', endpoint=name + 'named'),
+        ]
+        prefix = self.settings.get('VIEW_ATOM_FEED_PREFIX', '')
+        if prefix != '':
+            return [Submount('/' + prefix, url_rules)]
+        else:
+            return url_rules
+
+    def default(self, request):
+        return self.common(request, self.ap.feed.default())
+
+    def named(self, request, slug):
+        try:
+            feed = self.ap.feed.by_slug(slug)
+        except ValueError:
+            feed = None
+        if feed is None:
+            return Response('No feed with that name.', status=404,
+                    mimetype='text/plain')
+        return self.common(request, feed)
+
+    def common(self, request, feed):
+        content = xml.etree.tostring(feed.to_xml())
+        return Response(content, mimetype='application/atom+xml')
