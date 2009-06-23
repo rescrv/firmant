379541c0b6378f811d4d1fa271591f276d0e57a2
Robert Escriva <me@robescriva.com>
2009-06-23 14:38:54 -0400

    Removed last legacy frontend

--- firmant/frontend/jinja2/__init__.py
+++
@@ -1,8 +0,0 @@
-from firmant.plugins import resolver_list
-from firmant.frontend.jinja2.resolvers import Jinja2DateResolver
-from firmant.frontend.jinja2.resolvers import Jinja2EntryPermalink
-from firmant.configuration import settings
-
-
-def load():
-    resolver_list.append(Jinja2DateResolver(settings['FRONTEND_JINJA2_PREFIX']))
