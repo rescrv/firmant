cbab72e1eb8ad09e9b35425bb39f20a914b14921
Robert Escriva <me@robescriva.com>
2009-07-15 22:00:42 -0400

    Added SingleProviderPlugin.
    In an attempt to avoid import black magic, the PluginMount meta class is
    being replaced with explicit plugin lists in the configuration file.
    
    For the ViewProvider, a separate list of enabled plugins was provided on
    top of the plugin list.  This is the first in a series of steps designed
    to reduce import black magic and make the system more robust.

--- firmant/plugins/__init__.py
+++ firmant/plugins/__init__.py
@@ -19,3 +19,26 @@ class PluginMount(type):
             # Simply appending it to the list is all that's needed to keep
             # track of it later.
             cls.plugins.append(cls)
+
+class SingleProviderPlugin(object):
+
+    def __init__(self, rc, settings):
+        self.rc       = rc
+        self.settings = settings
+
+        full_path = settings.get(self.provider_setting, None)
+
+        if full_path is None:
+            raise ValueError('Please set "%s"', self.provider_setting)
+
+        module_name = '.'.join(full_path.split('.')[:-1])
+        object_name = full_path.split('.')[-1]
+        module      = get_module(module_name)
+
+        if not hasattr(module, object_name):
+            raise AttributeError('Module "%s" has no attribute "%s"' % \
+                    (module_name, object_name))
+
+        plugin = getattr(module, object_name)
+
+        self._provider = plugin(rc, settings)
