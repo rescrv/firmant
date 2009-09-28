803d18ae0aed7ca6fd6d87cc61413d6b4d3a6689
Robert Escriva <me@robescriva.com>
2009-09-27 23:18:40 -0400

    Another test case for view pagination.

--- test/plugins/views/generic.py
+++ test/plugins/views/generic.py
@@ -5,8 +5,8 @@ from firmant.plugins.views.generic import paginate
 
 class DummyUrls(object):
 
-    def build(endpoint, args, force_external):
-        return (endpoint, args, force_external)
+    def build(self, endpoint, args, force_external):
+        return (endpoint, args)
 
 
 rc    = {}
@@ -33,6 +33,23 @@ class TestPaginate(unittest.TestCase):
         self.assertEqual(page.has_older, False)
         self.assertEqual(page.newer, None)
         self.assertEqual(page.older, None)
+
+    def testPaginationBeginning(self):
+        '''firmant.plugins.views.generic.paginate
+        Tests the case where there is a first and (at least) a second page
+        because there are enough entries to require pagination.'''
+
+        func = lambda x, y: (range(1, 16)[y:x + y], 15 - (x + y))
+        page = 0
+        limit = 5
+
+        new_list, page = paginate(lambda: rc.copy(), limit, func, page)
+
+        self.assertEqual(range(1, 6), new_list)
+        self.assertEqual(page.has_newer, False)
+        self.assertEqual(page.has_older, True)
+        self.assertEqual(page.newer, None)
+        self.assertEqual(page.older, ('endpoint', {'args': True, 'page': 1}))
 
 
 from test import add_test
