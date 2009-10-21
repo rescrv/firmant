ef2d812788d74ad50fbe438e692a34b5fc4de4a8
Robert Escriva <me@robescriva.com>
2009-10-21 18:10:53 -0400

    Improve Storage coverage.

--- test/datasource/__init__.py
+++ test/datasource/__init__.py
@@ -65,7 +65,60 @@ def create_testDelete2(todelete, docstring, expt, check_func, *args, **kwargs):
     return testDelete2
 
 
+class TestStorage(unittest.TestCase):
+
+    class NewStorage1(Storage):
+
+        def _save(self, obj):
+            raise ValueError('save failed')
+
+        def _delete(self, obj):
+            raise ValueError('delete failed')
+
+    class NewStorage2(Storage):
+
+        def _save(self, obj):
+            raise Storage.StorageError('save failed')
+
+        def _delete(self, obj):
+            raise Storage.StorageError('delete failed')
+
+    def testSave1(self):
+        '''firmant.datasource.Storage.save'''
+
+        raised   = Storage.StorageError
+        function = lambda: TestStorage.NewStorage1().save(None)
+
+        self.assertRaises(raised, function)
+
+    def testSave2(self):
+        '''firmant.datasource.Storage.save'''
+
+        raised   = Storage.StorageError
+        function = lambda: TestStorage.NewStorage2().save(None)
+
+        self.assertRaises(raised, function)
+
+    def testDelete1(self):
+        '''firmant.datasource.Storage.delete'''
+
+        raised   = Storage.StorageError
+        function = lambda: TestStorage.NewStorage1().delete(None)
+
+        self.assertRaises(raised, function)
+
+    def testDelete2(self):
+        '''firmant.datasource.Storage.delete'''
+
+        raised   = Storage.StorageError
+        function = lambda: TestStorage.NewStorage2().delete(None)
+
+        self.assertRaises(raised, function)
+
+
+from test import add_test
 suite = unittest.TestSuite()
+add_test(suite, TestStorage)
 
 from test.datasource.atom import suite as atom_tests
 suite.addTests(atom_tests)
