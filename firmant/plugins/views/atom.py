78f2ea264555024754c2d7c0f8a1b8451120c619
Robert Escriva <me@robescriva.com>
2009-11-30 16:54:31 -0500

    Removed much of the atom generation code.
    Instead of implementing it here, it is implemented already as a contrib
    module in werkzeug.  Scrap this implementation in favor of the external
    implementation.  The external implementation seems to work, and is one
    less thing Firmant must maintain.

--- firmant/plugins/views/atom.py
+++ firmant/plugins/views/atom.py
@@ -1,9 +1,9 @@
-from werkzeug import Response
+from pytz import utc
+from werkzeug.contrib.atom import AtomFeed
+from werkzeug.contrib.atom import FeedEntry
 
 from firmant.views.generic import GenericFeedViewProvider
 from firmant.filters import FilterProvider
-from firmant.utils import xml
-from firmant.utils import RFC3339
 
 
 class AtomFeedPermalinkProvider(object):
@@ -34,90 +34,35 @@ class AtomFeedViewProvider(GenericFeedViewProvider):
     def render(self, request, feed):
         rc = self.rc()
         fp = rc.get(FilterProvider)
-        def filter(content):
-            open_div = '<div xmlns="http://www.w3.org/1999/xhtml">'
-            close_div = '</div>'
-            filtered_content = fp.filter('XHTML', content)
-            return xml.etree.fromstring(open_div + filtered_content + close_div)
-        content = xml.etree.tostring(feed_to_xml(feed, filter))
-        return Response(content, mimetype='application/atom+xml')
-
-
-def author_to_xml(author_obj):
-    author = xml.etree.Element('author')
-    xml.add_text_subelement(author, 'name',  author_obj.name)
-    xml.add_text_subelement(author, 'uri',   author_obj.uri)
-    xml.add_text_subelement(author, 'email', author_obj.email)
-    return author
-
-
-def category_to_xml(category_obj):
-    category = xml.etree.Element('category')
-    category.set('term',  category_obj.term)
-    category.set('label', category_obj.label)
-    return category
-
-
-def entry_to_xml(entry_obj, filter=None):
-    '''Convert an entry to XML.
-    Filter should be a function that will convert content to a valid etree
-    XML Element.'''
-
-    if filter is None:
-        def filter(content):
-            ret = xml.etree.Element('div')
-            ret.set('xmlns', 'http://www.w3.org/1999/xhtml')
-            ret.text = content
-            return ret
-    entry = xml.etree.Element('entry')
-
-    xml.add_text_subelement(entry, 'title',     entry_obj.title)
-    xml.add_text_subelement(entry, 'updated',   RFC3339(entry_obj.updated))
-    xml.add_text_subelement(entry, 'published', RFC3339(entry_obj.published))
-    author = author_to_xml(entry_obj.author)
-    entry.append(author)
-
-    content = xml.etree.SubElement(entry, 'content')
-    content.set('type', 'xhtml')
-    e = filter(entry_obj.content)
-    content.append(e)
-
-    l_alt = xml.etree.SubElement(entry, 'link')
-    l_alt.set('href', entry_obj.permalink)
-    l_alt.set('rel', 'alternate')
-
-    xml.add_text_subelement(entry, 'id', entry_obj.permalink)
-    xml.add_text_subelement(entry, 'rights', entry_obj.rights)
-
-    summary = xml.etree.SubElement(entry, 'summary')
-    summary.set('type', 'xhtml')
-    e = filter(entry_obj.summary)
-    summary.append(e)
-
-    category = category_to_xml(entry_obj.category)
-    entry.append(category)
-    return entry
-
-
-def feed_to_xml(feed_obj, filter=None):
-    '''Convert a feed to XML.
-    Filter should be a function that will convert content to a valid etree
-    XML Element.'''
-
-    feed = xml.etree.Element('feed')
-    feed.set('xmlns', 'http://www.w3.org/2005/Atom')
-
-    xml.add_text_subelement(feed, 'generator', 'Firmant')
-    xml.add_text_subelement(feed, 'title', feed_obj.title)
-    xml.add_text_subelement(feed, 'rights', feed_obj.rights)
-    xml.add_text_subelement(feed, 'updated', RFC3339(feed_obj.updated))
-    xml.add_text_subelement(feed, 'id', feed_obj.permalink)
-
-    link = xml.etree.SubElement(feed, 'link')
-    link.set('href', feed_obj.permalink)
-    link.set('rel', 'self')
-
-    for entry in feed_obj.entries:
-        feed.append(entry_to_xml(entry, filter))
-
-    return feed
+        params = {}
+        params['title'] = feed.title
+        params['title_type'] = 'text'
+        params['feed_url'] = params['id'] = feed.permalink
+        params['generator'] = ('Firmant', 'http://firmant.org/', None)
+        params['updated'] = feed.updated
+        params['rights'] = feed.rights
+        params['rights_type'] = 'text'
+        params['subtitle'] = feed.subtitle
+        params['subtitle_type'] = 'text'
+        af = AtomFeed(**params)
+        for entry in feed.entries:
+            params = {}
+            params['title'] = entry.title
+            params['title_type'] = 'text'
+            params['content'] = fp.filter('XHTML', entry.content)
+            params['content_type'] = 'xhtml'
+            params['summary'] = fp.filter('XHTML', entry.summary)
+            params['summary_type'] = 'xhtml'
+            params['author'] = {}
+            params['author']['name'] = entry.author.name
+            if entry.author.uri is not None:
+                params['author']['uri'] = entry.author.uri
+            if entry.author.email is not None:
+                params['author']['email'] = entry.author.email
+            params['rights'] = entry.rights
+            params['rights_type'] = 'text'
+            params['url'] = params['id'] = entry.permalink
+            params['updated'] = entry.updated.astimezone(utc)
+            params['published'] = entry.published.astimezone(utc)
+            af.add(FeedEntry(**params))
+        return af.get_response()
