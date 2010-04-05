4f08bb7d7d9992e73e3f9240adeacdbcef7d9b48
Robert Escriva <me@robescriva.com>
2010-04-04 21:59:46 -0400

    Add setup.py file.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Firmant',
+        version='0.1',
+        description='Static Blog Generator',
+        author='Robert Escriva',
+        author_email='projects@mail.robescriva.com',
+        url='http://projects.robescriva.com/projects/show/firmant',
+        packages=['firmant'],
+        )
