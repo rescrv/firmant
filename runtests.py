072338512b55f3243f56d072b2d89565a097c901
Robert Escriva <me@robescriva.com>
2011-10-17 15:32:40 -0400

    Update the test runner to only run new stuff.

--- runtests.py
+++ runtests.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python
 
 import gettext
+import importlib
 import logging
 import unittest
 import doctest
 import sys
 from optparse import OptionParser
-
-from minimock import Mock
-from pprint import pprint
-from pysettings.modules import get_module
 
 
 gettext.install('firmant')
@@ -24,60 +21,19 @@ def safe_displayhook(s):
 sys.displayhook = safe_displayhook
 
 
-def get_logger():
-    logger = logging.getLogger('logger')
-    logger.setLevel(logging.WARN)
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
-    handler.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
-    return logger
-
-
 if __name__ == '__main__':
     suite = unittest.TestSuite()
 
-    modules = ['firmant'
-              ,'firmant.application'
-              ,'firmant.chunks'
-              ,'firmant.decorators'
-              ,'firmant.du'
-              ,'firmant.globs'
-              ,'firmant.paginate'
-              ,'firmant.parsers'
-              ,'firmant.parsers.feeds'
-              ,'firmant.parsers.posts'
-              ,'firmant.parsers.static'
-              ,'firmant.parsers.staticrst'
-              ,'firmant.parsers.tags'
-              ,'firmant.plugin'
-              ,'firmant.routing'
-              ,'firmant.routing.components'
-              ,'firmant.settings'
+    modules = ['firmant.objects'
               ,'firmant.urls'
-              ,'firmant.utils'
-              ,'firmant.utils.exceptions'
-              ,'firmant.utils.paths'
-              ,'firmant.utils.workarounds'
-              ,'firmant.writers'
-              ,'firmant.writers.atom'
-              ,'firmant.writers.feeds'
-              ,'firmant.writers.j2'
-              ,'firmant.writers.posts'
-              ,'firmant.writers.static'
-              ,'firmant.writers.staticrst'
               ]
 
     if len(sys.argv[1:]) > 0:
         modules = sys.argv[1:]
 
     for module in modules:
-        mod = get_module(module)
+        mod = importlib.import_module(module)
         args = {}
-        extraglobs = {'Mock': Mock
-                     ,'pprint': pprint
-                     ,'get_logger': get_logger
-                     }
         for arg, attr in [('module_relative', '_module_relative')
                          ,('package', '_package')
                          ,('setUp', '_setup')
@@ -89,8 +45,6 @@ if __name__ == '__main__':
                          ]:
             if hasattr(mod, attr):
                 args[arg] = getattr(mod, attr)
-        extraglobs.update(args.get('extraglobs', dict()))
-        args['extraglobs'] = extraglobs
         suite.addTest(doctest.DocTestSuite(mod, **args))
 
     results = unittest.TextTestRunner(verbosity=2).run(suite)
