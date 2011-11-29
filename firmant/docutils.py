d9eff2a40f7c53726e5b92748e4f9eccd5d7859f
Robert Escriva <me@robescriva.com>
2011-11-29 11:41:57 -0500

    Add CSS class to URLs that identify the type.
    The CSS class that is added to the urls uniquely identifies the set of
    attributes used to construct the URL.

--- firmant/docutils.py
+++ firmant/docutils.py
@@ -132,6 +132,7 @@ class RestDocument(object):
                 attrs = dict(attrs)
                 url = firmant.urls.url(attrs)
                 if url:
+                    reference.set_class('-'.join(['url'] + sorted(attrs.keys())))
                     reference.attributes['refuri'] = url
         for image in self._pub.document.traverse(docutils.nodes.image):
             url = firmant.urls.url({'image': image.get('uri')})
